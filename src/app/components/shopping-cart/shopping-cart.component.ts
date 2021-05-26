0f685fa3f7c3f19251626f0b36e029ced72dfbb0
Melina Kamyab <melina.kamyab@gmail.com>
2021-05-26 17:11:45 +0200

    add similar cart items so that they get a total sum

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -9,15 +9,40 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class ShoppingCartComponent implements OnInit {
   cartItems: Movie[] = [];
+  updatedCart: Movie[] = [];
   
   constructor(private service: CartService) { }
 
+
   ngOnInit(): void {
+    //get the cartItems from our service and subscribe on it
     this.service.cartItems$.subscribe((data) => {
       this.cartItems = data;
     })
     this.service.getCartItems();
-    console.log(this.cartItems)
+  }
+
+  handleCartItems(){
+    //create a holder for when looping through cartItems
+    let holder = {};
+
+    //check for similar objects and add the prices of the similar objects together
+    this.cartItems.forEach(function(d){
+      if (holder.hasOwnProperty(d.name)){
+        holder[d.name] = holder[d.name] + d.price;
+      }else {
+        holder[d.name] = d.price
+      }
+    })
+    
+    //create a new cart-array and place the objects inside it 
+    let updatedCart = [];
+    for(let prop in holder){
+      updatedCart.push({name:prop, price:holder[prop]});
+    }
+    console.log(updatedCart)
+    //return value
+    return updatedCart;
   }
 
 }
